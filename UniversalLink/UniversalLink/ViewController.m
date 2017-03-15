c4a8cec66de68b00525f5c4533f5afbd2bd132cb
李亚坤 <285577079@qq.com>
2017-03-15 10:50:09 +0800

    First Blood!

---
+++ UniversalLink/UniversalLink/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UniversalLink
+//
+//  Created by 李亚坤 on 2017/3/15.
+//  Copyright © 2017年 chinaNews. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
